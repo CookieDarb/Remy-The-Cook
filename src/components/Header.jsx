527eff6f130265f5efcd0b2d80ab2d01e63067de
CookieDarb <darkblueauror@gmail.com>
2025-01-01 01:41:32 +0530

    Header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import '/src/style/Header.css'
+
+export default function Header(){
+    return(
+        <header>
+            <img src="images/logo.png" alt="Remy The Cook logo" width="60"/>
+            <p>Remy The Cook</p>
+        </header>
+    )
+}
