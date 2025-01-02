0e4f458c1ef9eb29128bcad17e0ab8a0a01f687d
CookieDarb <darkblueauror@gmail.com>
2025-01-02 23:38:05 +0530

    backing up before react update to fix form action

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,20 @@
 import '/src/style/Main.css'
+import React from "react"
 
 export default function Main(){
+
+    const [ingredients,setIngredients]=React.useState(["Jam", "Bread"])
+
+    const ingredientsArray=ingredients.map(item=>{
+        return <li key={item}>{item}</li>
+    })
+
+    function addIngredient(formData){
+        const newIngredient=formData.get("ingredient")
+        setIngredients(prevIngredients=>[...prevIngredients, newIngredient])
+        console.log(formData)
+    }
+
     return(
         <main>
             
@@ -12,10 +26,14 @@ export default function Main(){
                 <p>Oh, welcome! I guess you're here for a little help whipping up something delicious, huh? Great! Just list the ingredients you’ve got down below, and we’ll cook up some magic!</p>
             </div>
 
-            <form>
-                <input type="text" placeholder="e.g. Carrots" aria-label="Add ingredients" />
+            <form action={addIngredient}>
+                <input type="text" placeholder="e.g. Carrots" aria-label="Add ingredients" name="ingredient" id="ingredient"/>
                 <button type='submit'>+ Add</button>
             </form>
+
+            <ul>
+                {ingredientsArray}
+            </ul>
         </main>
     )
 }
